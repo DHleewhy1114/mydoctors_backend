6679bac13aa168fe1a766a89aecff352415377b8
dh_leewhy <dlehd14@gmail.com>
2018-08-11 01:58:59 +0900

    setup.py 변경

--- app/setup.py
+++ app/setup.py
@@ -15,20 +15,3 @@ logging.basicConfig(
 if __name__ == '__main__':
     #log.info('Create database {}'.format(base.db_name))
     models.Base.metadata.create_all(models.engine)
-"""
-    log.info('Insert Planet data in database')
-    with open('database/data/planet.json', 'r') as file:
-        data = literal_eval(file.read())
-        for record in data:
-            planet = ModelPlanet(**record)
-            base.db_session.add(planet)
-        base.db_session.commit()
-
-    log.info('Insert People data in database')
-    with open('database/data/people.json', 'r') as file:
-        data = literal_eval(file.read())
-        for record in data:
-            planet = ModelPeople(**record)
-            base.db_session.add(planet)
-        base.db_session.commit()
-"""
